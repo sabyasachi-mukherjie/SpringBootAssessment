574ffc7a1a9d05927ff15ffffc044c68d1932dc0
11706111 <61941561+11706111@users.noreply.github.com>
2020-03-12 20:38:03 +0530

    Removed unused imports

--- UserAddClient/src/main/java/com/accenture/sabyasachi/controller/UserController.java
+++ UserAddClient/src/main/java/com/accenture/sabyasachi/controller/UserController.java
@@ -1,13 +1,10 @@
 package com.accenture.sabyasachi.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
--- UserAddClient/src/main/java/com/accenture/sabyasachi/service/UserService.java
+++ UserAddClient/src/main/java/com/accenture/sabyasachi/service/UserService.java
@@ -1,9 +1,5 @@
 package com.accenture.sabyasachi.service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
